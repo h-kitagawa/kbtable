d879916c254193eb36984c2922411273d3b0c109
Hironori Kitagawa <h_kitagawa2001@yahoo.co.jp>
2015-01-14 15:51:11 +0900

    \expandafter

---
+++ .emacs.el
@@ -0,0 +1,6 @@
+; 6段キーボード
+(global-set-key (kbd "C-<print>") '(lambda () (interactive) (insert "\\expandafter")))
+
+; 7段キーボード
+(global-set-key (kbd "C-<menu>") '(lambda () (interactive) (insert "\\expandafter")))
+
